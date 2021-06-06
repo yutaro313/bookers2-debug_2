befc217bf27ccad55f62ea621741e3fef8ad77b2
yutaro313 <yuutarou55@icloud.com>
2021-06-06 14:46:46 +0000

    [Update]lssue6

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
     @model = params["search"]["model"]
     @value = params["search"]["value"]
     @how = params["search"]["how"]
-    @dates = search_for(@how, @model, @value)
+    @datas = search_for(@how, @model, @value)
   end
 
   private
